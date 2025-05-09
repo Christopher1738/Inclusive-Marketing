68c0e994aab1bb7f1a1742d159184c7cb677a16b
Christopher1738 <jabuchris1@gmail.com>
2025-05-09 11:13:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Slider Functionality
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Auto-rotate the slides every 3 seconds
